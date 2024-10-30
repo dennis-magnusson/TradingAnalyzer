5a40d83f8d8ccec4ad30d19de0282aa026f70cfc
Dennis Magnusson <magnden@gmail.com>
2024-10-30 15:28:00 +0200

    WIP: working manually

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ Low-latency platform for real-time trading analysis.
 
 -   `sbt`
 -   `docker` and `docker compose`
+-   Download the DEBS 2022 Grand Challenge dataset's csv files ([link](https://zenodo.org/records/6382482)) and place them in a directory `./data`.
 
 1. Compile the data_producer scala project
 
@@ -19,10 +20,31 @@ cd ..
 
 This will compile a .jar file for the data_producer docker container.
 
-2. Download the DEBS 2022 Grand Challenge dataset's csv files ([link](https://zenodo.org/records/6382482)) and place them in a directory `./data`.
+2.  Run the containers
+    `docker compose up`
 
-3. Run the containers
-   `docker compose up`
+### Manual use (temporary)
+
+1. Start Kafka in a Docker container
+
+`docker run -p 9092:9092 apache/kafka:3.8.1`
+
+2. Compile and build the data_producer
+
+```
+cd data_producer
+sbt assembly
+cd ..
+java -jar data_producer/target/scala-2.12/dataproducer-assembly-1.0.jar
+```
+
+3. Consume the events from the topic with the kafka console consumer. [Download](https://dlcdn.apache.org/kafka/3.8.1/kafka_2.13-3.8.1.tgz) the Kafka release here.
+
+```
+tar -xzf kafka_2.13-3.8.1.tgz
+cd kafka_2.13-3.8.1
+bin/kafka-console-consumer.sh --topic trade-events --bootstrap-server localhost:9092 --from-beginning
+```
 
 ## Liscense
 
--- data_producer/src/main/scala/DataGenerator.scala
+++ data_producer/src/main/scala/DataGenerator.scala
@@ -1,11 +1,50 @@
 import java.io.File
-import java.time._
 import com.github.tototoshi.csv._
+import org.apache.kafka.clients.admin.{AdminClient, NewTopic}
+import org.apache.kafka.clients.producer.{
+  KafkaProducer,
+  ProducerConfig,
+  ProducerRecord
+}
+import java.util.{Properties, Collections}
 
 object DataGenerator extends App {
 
-  val path = "/data/debs2022-gc-trading-day-08-11-21.csv"
-  val file = new File(path)
+  val INTERVAL_MS = 500 // TODO: Make this dynamically configurable
+  val CSV_PATH =
+    "./data/debs2022-gc-trading-day-08-11-21.csv" // TODO: What about other days' data?
+
+  val kafkaServer =
+    "localhost:9092" // TODO: Point to the Kafka container
+
+  val adminClientProps = new Properties()
+  adminClientProps.put(
+    "bootstrap.servers",
+    kafkaServer
+  )
+  val adminClient = AdminClient.create(adminClientProps)
+
+  val topicName = "trade-events"
+  val partitions = 1
+  val replicationFactor: Short = 1
+  val topic = new NewTopic(topicName, partitions, replicationFactor)
+  val topicCollection = Collections.singleton(topic)
+  adminClient.createTopics(topicCollection)
+
+  val producerProps = new Properties()
+  producerProps.put("bootstrap.servers", kafkaServer)
+  producerProps.put(
+    "key.serializer",
+    "org.apache.kafka.common.serialization.StringSerializer"
+  )
+  producerProps.put(
+    "value.serializer",
+    "org.apache.kafka.common.serialization.StringSerializer"
+  )
+
+  val producer = new KafkaProducer[String, String](producerProps)
+
+  val file = new File(CSV_PATH)
   val reader = CSVReader.open(file)
 
   var row = reader.readNext()
@@ -13,11 +52,11 @@ object DataGenerator extends App {
   while (row.isDefined) {
     val values = row.get
 
-    // TODO: Actually write the event to Kafka
+    val record =
+      new ProducerRecord[String, String](topicName, values.mkString(","))
+    producer.send(record)
 
-    println(values.mkString(","))
-
-    Thread.sleep(500)
+    Thread.sleep(INTERVAL_MS)
     row = reader.readNext()
   }
 
