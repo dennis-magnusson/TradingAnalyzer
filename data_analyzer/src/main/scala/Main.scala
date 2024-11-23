9290a02c07fee1c3f7579d9b4cc76fee855e753e
Sahel <mesforoshs@gmail.com>
2024-11-23 11:13:40 +0200

    Use key as symbol

--- data_analyzer/src/main/scala/Main.scala
+++ data_analyzer/src/main/scala/Main.scala
@@ -11,13 +11,15 @@ object Main extends App {
 import spark.implicits._
 
 val df = spark.readStream.format("kafka").option("kafka.bootstrap.servers", "kafka:9092").option("subscribe", "trade-events").option("startingOffsets","earliest").load()
-val cleandf = df.selectExpr("cast(key as string) key", "cast(value as string) value", "timestamp").select(split(col("value"),",").getItem(1).as("symbol"),
+val cleandf = df.selectExpr("cast(key as string) key", "cast(value as string) value", "timestamp").select(
     split(col("value"),",").getItem(2).as("trading_value"),
     split(col("value"),",").getItem(3).as("tradingtime"), col("key"), col("timestamp"))
 
-val output = cleandf.select(col("symbol").as("key"), concat($"trading_value", lit(","), $"tradingtime", lit(","), $"timestamp").as("value"))
+val output = cleandf.select(col("key"), concat($"trading_value", lit(","), $"tradingtime", lit(","), $"timestamp").as("value"))
 
-output.writeStream.format("kafka").option("checkpointLocation" , "./spark").option("kafka.bootstrap.servers", "kafka:9092").option("topic", "topic1").start().awaitTermination()
+// output.writeStream.format("kafka").option("checkpointLocation" , "./spark").option("kafka.bootstrap.servers", "kafka:9092").option("topic", "topic1").start()
+
+
 
 
 }
