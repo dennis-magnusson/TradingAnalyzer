e5f1181dc3e594f768938bd6d55d28d120402cf1
Dennis Magnusson <magnden@gmail.com>
2024-12-02 14:07:26 +0200

    add env variables for kafka configurations

--- data_analyzer/src/main/scala/Main.scala
+++ data_analyzer/src/main/scala/Main.scala
@@ -1,25 +1,51 @@
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.SparkSession
 
-
 object Main extends App {
-    val spark = SparkSession
-  .builder
-  .appName("StreaminSample")
-  .getOrCreate()
 
-import spark.implicits._
+  val latencyTopicName: String =
+    sys.env.getOrElse("LATENCY_TOPIC_NAME", "timestamps")
+  val tradeEventsTopicName: String =
+    sys.env.getOrElse("TRADE_EVENTS_TOPIC_NAME", "trade-events")
+  val kafkaServer: String = "kafka:9092"
 
-val df = spark.readStream.format("kafka").option("kafka.bootstrap.servers", "kafka:9092").option("subscribe", "trade-events").option("startingOffsets","earliest").load()
-val cleandf = df.selectExpr("cast(key as string) key", "cast(value as string) value", "timestamp").select(
-    split(col("value"),",").getItem(2).as("trading_value"),
-    split(col("value"),",").getItem(3).as("tradingtime"), col("key"), col("timestamp"))
+  val spark = SparkSession.builder
+    .appName("StreaminSample")
+    .getOrCreate()
 
-val output = cleandf.select(col("key"), concat($"trading_value", lit(","), $"tradingtime", lit(","), $"timestamp").as("value"))
+  import spark.implicits._
 
-output.writeStream.format("kafka").option("checkpointLocation" , "./spark").option("kafka.bootstrap.servers", "kafka:9092").option("topic", "topic1").start().awaitTermination()
+  val df = spark.readStream
+    .format("kafka")
+    .option("kafka.bootstrap.servers", kafkaServer)
+    .option("subscribe", tradeEventsTopicName)
+    .option("startingOffsets", "earliest")
+    .load()
+  val cleandf = df
+    .selectExpr(
+      "cast(key as string) key",
+      "cast(value as string) value",
+      "timestamp"
+    )
+    .select(
+      split(col("value"), ",").getItem(2).as("trading_value"),
+      split(col("value"), ",").getItem(3).as("tradingtime"),
+      col("key"),
+      col("timestamp")
+    )
 
+  val output = cleandf.select(
+    col("key"),
+    concat($"trading_value", lit(","), $"tradingtime", lit(","), $"timestamp")
+      .as("value")
+  )
 
-
+  output.writeStream
+    .format("kafka")
+    .option("checkpointLocation", "./spark")
+    .option("kafka.bootstrap.servers", kafkaServer)
+    .option("topic", latencyTopicName)
+    .start()
+    .awaitTermination()
 
 }
