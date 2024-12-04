a0a03dd9cda1963835a9320a27b7be5aefec1acb
Dennis Magnusson <magnden@gmail.com>
2024-12-04 20:04:25 +0200

    add creation of another topic for buy/sell advisories

--- kafka-init-topics.sh
+++ kafka-init-topics.sh
@@ -8,6 +8,10 @@ LATENCY_TOPIC_NAME=${LATENCY_TOPIC_NAME:-timestamps}
 LATENCY_PARTITIONS=${LATENCY_PARTITIONS:-1}
 LATENCY_REPLICATION_FACTOR=${LATENCY_REPLICATION_FACTOR:-1}
 
+ADVISORY_TOPIC_NAME=${LATENCY_TOPIC_NAME:-timestamps}
+ADVISORY_PARTITIONS=${LATENCY_PARTITIONS:-1}
+ADVISORY_REPLICATION_FACTOR=${LATENCY_REPLICATION_FACTOR:-1}
+
 BOOTSTRAP_SERVER=${BOOTSTRAP_SERVER:-kafka:9092}
 
 /opt/kafka/bin/kafka-topics.sh --create \
@@ -20,6 +24,12 @@ BOOTSTRAP_SERVER=${BOOTSTRAP_SERVER:-kafka:9092}
     --topic "$LATENCY_TOPIC_NAME" \
     --partitions "$LATENCY_PARTITIONS" \
     --replication-factor "$LATENCY_REPLICATION_FACTOR" \
+    --bootstrap-server "$BOOTSTRAP_SERVER"
+
+/opt/kafka/bin/kafka-topics.sh --create \
+    --topic "$ADVISORY_TOPIC_NAME" \
+    --partitions "$ADVISORY_PARTITIONS" \
+    --replication-factor "$ADVISORY_REPLICATION_FACTOR" \
     --bootstrap-server "$BOOTSTRAP_SERVER"
 
 # TODO: make script succesfully exit even if the topics are already created
