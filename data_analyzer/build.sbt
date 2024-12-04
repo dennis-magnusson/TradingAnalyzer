f26e87a73228e76ab08e3d90d114d4d72ea501a6
Dennis Magnusson <magnden@gmail.com>
2024-12-04 10:02:13 +0200

    wip: try implementing windowed aggregations in kafka streams

--- data_analyzer/build.sbt
+++ data_analyzer/build.sbt
@@ -3,5 +3,11 @@ scalaVersion := "2.12.20"
 name := "dataanalyzer"
 version := "1.0"
 
-// libraryDependencies += "org.apache.kafka" % "kafka-streams" % "3.8.1"
+libraryDependencies += "org.apache.kafka" % "kafka-streams" % "3.8.1"
 libraryDependencies += "org.apache.kafka" % "kafka-clients" % "3.8.1"
+
+assemblyMergeStrategy in assembly := {
+  case PathList("META-INF", _ @_*) => MergeStrategy.discard
+  case "module-info.class"         => MergeStrategy.discard
+  case x                           => MergeStrategy.first
+}
--- data_analyzer/src/main/scala/Main.scala
+++ data_analyzer/src/main/scala/Main.scala
@@ -1,6 +1,13 @@
-import org.apache.kafka.streams.{KafkaStreams, StreamsBuilder, StreamsConfig}
-import org.apache.kafka.streams.kstream.{TimeWindows, Materialized}
+import org.apache.kafka.streams.{StreamsBuilder, KeyValue}
+import org.apache.kafka.streams.kstream.{
+  TimeWindows,
+  TimeWindowedKStream,
+  Produced,
+  KeyValueMapper,
+  Windowed
+}
 import org.apache.kafka.common.serialization.Serdes
+import org.apache.kafka.streams.{KafkaStreams, StreamsConfig}
 import java.time.Duration
 import java.util.Properties
 
@@ -10,8 +17,12 @@ object KafkaStreamProcessor extends App {
   val readTopicName = sys.env.getOrElse("READ_TOPIC_NAME", "trade-events")
   val kafkaServer = sys.env.getOrElse("KAFKA_SERVER", "kafka:9092")
 
+  // read from readTopicName on kafkaServer, aggregate a 5 minute tumbling window, write something for each window to writeTopicName
+
+  // the events in readTopicName are key: [String], value: [String]
+
   val props = new Properties()
-  props.put(StreamsConfig.APPLICATION_ID_CONFIG, "trading-analyzer")
+  props.put(StreamsConfig.APPLICATION_ID_CONFIG, "kafka-stream-processor")
   props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServer)
   props.put(
     StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG,
@@ -23,7 +34,42 @@ object KafkaStreamProcessor extends App {
   )
 
   val builder = new StreamsBuilder()
-  val tradingEvents = builder.stream[String, String](readTopicName)
 
-  println("Hello World")
+  val windowed: TimeWindowedKStream[String, String] =
+    builder
+      .stream[String, String](readTopicName)
+      .groupByKey()
+      .windowedBy(TimeWindows.of(Duration.ofMinutes(1)))
+      .aggregate(
+        () => (0.0, 0),
+        (key: String, value: String, aggregate: (Double, Int)) => {
+          val (sum, count) = aggregate
+          val lastTradePrice = value.split(",")(1).toDouble
+          (sum + lastTradePrice, count + 1)
+        }
+      )
+      .toStream()
+      .foreach((key, value) => {
+        println(
+          s"Key: ${key.key()} Window start: ${key
+              .window()
+              .start()}, Window end: ${key.window().end()}, Count: $value"
+        )
+      })
+
+  val a: Nothing = windowed
+
+  // counts.foreach((key, value) => {
+  //   println(
+  //     s"Key: ${key.key()} Window start: ${key.window().start()}, Window end: ${key.window().end()}, Count: $value"
+  //   )
+  // })
+
+  val producedInstance =
+    Produced.`with`(Serdes.String(), Serdes.String())
+
+  val topology = builder.build()
+
+  val streams = new KafkaStreams(topology, props)
+  streams.start()
 }
